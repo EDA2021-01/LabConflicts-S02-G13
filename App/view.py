a0dd6582119afa9527d9b77a069dd718eb193d99
Jesed Dominguez <j.dominguezp@uniandes.edu.co>
2021-02-10 16:28:54 -0500

    Cambios estudiante 2.1

--- App/view.py
+++ App/view.py
@@ -89,7 +89,7 @@ while True:
     elif True:
         # TO-DO: Modificación de Est-2 en el Lab 2
         pass
-    elif int(input[3]) == 3:
+    elif int(inputs[3]) == 3:
         print("Cargando información de Book-Tags...")
         booktags = loadBookTags()
         print("Total de Book-Tags cargados: "+str(lt.size(booktags)))
